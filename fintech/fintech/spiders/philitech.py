1151ff9afc5a54bdec8ad226cce948ddd4aee0e7
Bayaniblues <cortez.ethridge@gmail.com>
2020-11-01 20:59:23 -0700

    added module2

--- fintech/fintech/spiders/philitech.py
+++ fintech/fintech/spiders/philitech.py
@@ -4,19 +4,19 @@ from ..items import FintechItem
 
 class PhilitechSpider(scrapy.Spider):
     name = 'philitech'
-    allowed_domains = ['https://fintechnews.ph/fintech-startups-philippines/']
-    start_urls = ['https://fintechnews.ph/fintech-startups-philippines/']
+    allowed_domains = ['https://entreplounge.com/100-of-the-best-startups-in-the-philippines/']
+    start_urls = ['https://entreplounge.com/100-of-the-best-startups-in-the-philippines/']
 
     def parse(self, response):
         # call items
         items = FintechItem()
 
         # extract from css
-        name = response.css('p+ h1').extract()
-        description = response.css(".column6:nth-child(1, 2)").extract()
+        # name = response.css('p+ h1').extract()
+        description = response.css("p:nth-child(28) , p:nth-child(26) , h1~ p+ p").extract()
 
         # store our results
-        items["Name"] = name
+        # items["Name"] = name
         items['description'] = description
 
         yield items
